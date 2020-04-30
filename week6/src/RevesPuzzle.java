2b2e52903eafca65a8912ed7084508f530be26d9
chaofeng <fc2002002002@hotmail.com>
2020-04-30 16:32:08 -0700

    add branch

--- week6/src/RevesPuzzle.java
+++ week6/src/RevesPuzzle.java
@@ -1,14 +1,46 @@
 public class RevesPuzzle {
-    public static String hanoi(int n, boolean left) {
-        if (n == 0) return " ";
+    private static void hanoi(int n, int k, String[] slots) {
+        int[] now = new int[n-k+1];
+        for (int i = 0; i < n-k+1; i++) {
+            now[i] = 1;
+        }
+
+        System.out.println(hanoi(n, k, false, slots, now) );
+    }
+    private static String hanoi(int n, int k, boolean left, String[] slots, int[] now) {
+
+        if (n == k) return " ";
         String move;
-        if (left) move = n + "L";
-        else move = n + "R";
-        return hanoi(n-1, !left) + move + hanoi(n-1, !left);
+        int current = 0, next = 0;
+        if (left) {
+            current = now[n-k];
+            next = (now[n-k]-1 + slots.length) % slots.length;
+            now[n-k] = next;
+        }
+        else {
+            current = now[n-k];
+            next = (now[n-k] + 1)  % slots.length;
+            now[n-k] = next;
+        }
+        move = "Move disc " + n + " from " + slots[current] + " to " + slots[next] + "\n";
+
+        return hanoi(n-1, k, !left, slots, now) + move + hanoi(n-1, k, !left, slots, now);
+    }
+    private static void reves(int n, int m, String[] slots) {
+
     }
 
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        StdOut.println(hanoi(n, false));
+        int k = (int) Math.round(n+1 - Math.sqrt(n+n+1));
+        System.out.println("n = " + n + ", k = " + k);
+        String[] slots = {"C", "A", "B"};
+        hanoi(k, 0, slots);
+        slots = new String[]{"C", "A", "D"};
+        hanoi(n, k, slots);
+        slots = new String[]{"C", "B", "D"};
+        hanoi(k, 0, slots);
+
+
     }
 }
--- week6/src/TrinomialDP.java
+++ week6/src/TrinomialDP.java
@@ -3,14 +3,9 @@ public class TrinomialDP {
     // Returns the trinomial coefficient T(n, k).
     static long[][] dp;
     public static long trinomial(int n, int k) {
-        if (k < 0 || k > 2*n) return 0;
+        if (n == 0 || k == 0) return 1;
+        if (k < -n || k > n) return 0;
 
-        if (n == 0 && k == 0) return 1;
-
-        if (n == 1) {
-            if (k <= 2) return 1;
-            else return 0;
-        }
         if (dp[n][k] == 0) dp[n][k] = trinomial(n-1, k) + trinomial(n-1, k-1) + trinomial(n-1, k-2);
         //System.out.println("("+n + ", " + k + ")" + dp[n][k]);
         return dp[n][k];
@@ -20,13 +15,7 @@ public class TrinomialDP {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int k = Integer.parseInt(args[1]);
-        if (n * (n+k) < 0) {
-            System.out.println("0");
-            return;
-        }
-        n = Math.abs(n);
-        k = Math.abs(k);
-        dp = new long[n+1][2*(k+n)+1];
-        System.out.println(trinomial(n, k+n));
+
+        System.out.println(trinomial(n, k));
     }
 }

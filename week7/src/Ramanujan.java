89cf0160180bf0195bcad85faa466763ed4d7b9e
chaofeng <fc2002002002@hotmail.com>
2020-05-02 16:42:22 -0700

    add week7

--- week7/src/Ramanujan.java
+++ week7/src/Ramanujan.java
@@ -2,31 +2,36 @@ public class Ramanujan {
 
     // Is n a Ramanujan number?
     public static boolean isRamanujan(long n) {
+        if (n <= 0) return false;
         int maxCount = (int)Math.cbrt(n)+2;
         long[] array = new long[maxCount];
         for (int i = 0; i < array.length; i++) {
-            array[i] = i * i * i;
+            array[i] = (long)i * i * i;
         }
         int left = 0, right = array.length - 1;
-        while (left <= right) {
-            if (array[left] + array[right] == n) {
-                System.out.println("one ok");
+        while (left < right) {
+            int result = Long.compareUnsigned(array[left] + array[right], n);
+            if (result == 0)  {
                 int l = left +1, r = right - 1;
-                while (l <= r) {
-                    if (array[l] + array[r] == n) {
+                while (l < r) {
+                    int result2 = Long.compareUnsigned(array[l] + array[r], n);
+
+                    if (result2 == 0) {
                         return true;
-                    } else if (array[l] + array[r] > n) {
+                    } else if (result2 > 0) {
                         r --;
                     } else {
                         l ++;
                     }
                 }
-            } else if (array[left] + array[right] > n) {
+                right--;
+                left++;
+            } else if (result > 0) {
                 right --;
             } else {
                 left ++;
             }
-            System.out.println(left + ", " + right);
+//            System.out.println(left + ", " + right);
         }
         return false;
     }
@@ -34,9 +39,13 @@ public class Ramanujan {
     // Takes a long integer command-line arguments n and prints true if
     // n is a Ramanujan number, and false otherwise.
     public static void main(String[] args) {
-        long n = Long.parseLong(args[0]);
+        long n = Long.parseUnsignedLong(args[0]);
         System.out.println(isRamanujan(n));
-        System.out.println(Math.cbrt(n) + "  " + n);
-        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(isRamanujan(1729));
+//        System.out.println(isRamanujan(3458));
+//        System.out.println(isRamanujan(4104));
+//        System.out.println(isRamanujan(216125));
+
+
     }
 }
---
+++ week8/src/Huntingtons.java
@@ -0,0 +1,25 @@
+public class Huntingtons {
+
+    // Returns the maximum number of consecutive repeats of CAG in the DNA string.
+    public static int maxRepeats(String dna) {
+
+    }
+
+    // Returns a copy of s, with all whitespace (spaces, tabs, and newlines) removed.
+    public static String removeWhitespace(String s) {
+        s = s.replaceAll("\\s|\t|\n", s);
+        return s;
+    }
+
+    // Returns one of these diagnoses corresponding to the maximum number of repeats:
+    // "not human", "normal", "high risk", or "Huntington's".
+    public static String diagnose(int maxRepeats) {
+
+    }
+
+    // Sample client (see below).
+    public static void main(String[] args) {
+
+    }
+
+}
